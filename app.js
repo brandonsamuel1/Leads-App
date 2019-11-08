d99c2961cce0181cc0cf781ead2a0d09f2510e94
Brandon <brandonsamuel72@gmail.com>
2019-11-07 23:29:33 -0800

    created db and connection to app

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const Sequelize = require('sequelize');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+
+const sequelize = new Sequelize(process.env.DB_CONNECT, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database: ', err);
+  });
 
 app.get('/', (req, res) => {
     res.send('Hello')
